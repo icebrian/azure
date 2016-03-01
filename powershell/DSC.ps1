fd7fd38279c9d55889b95b2e564f125ece827bae
Istvan Cebrian <istvan.cebrian@gmail.com>
2016-03-01 17:30:53 +0000

    additions

--- powershell/DSC.ps1
+++ powershell/DSC.ps1
@@ -1,4 +1,4 @@
-﻿
+
 # Repositories: Chocolatey + PowerShell Gallery
 
 # Find all Modules
@@ -13,7 +13,6 @@ Find-DscResource -ov r | measure
 # Grid View of contents of var $r
 $r | ogv
 
-
 ################################################################## mva START
 # Find all MSFT Virtual Academy modules and pipe to install
 Find-Module mva* |Install-Module
@@ -23,5 +22,17 @@ Find-Module mva* |Install-Module
 gmo -ListAvailable mva*
 
 # Load examples
-Show-MVA_DSC_Examples -Day 1 -Module 2
+Show-MVA_DSC_Examples -Day 1 -Module 3
 ################################################################## mva END
+
+Get-Help *-DSC*
+Get-Help *localConfig*
+Get-DscLocalConfigurationManager -CimSession s1
+
+# Get DSC Resources
+Get-DscResource
+Get-DscResource -Name WindowsFeature | Select-Object -ExpandProperty properties
+Get-DscResource -Name WindowsFeature -Syntax
+
+# Get Windows Feature and Pipe to Out Grid View
+Get-WindowsOptionalFeature -Online | ogv
---
+++ powershell/Exec-template-with-Extension.ps1
@@ -0,0 +1,28 @@
+######################################################################################
+# Script: 2iis-1sql
+# Description: Uses Template file to create 1 Windows Server with Puppet Agent installed
+# for further information, please refer to:
+# - https://azure.microsoft.com/en-us/documentation/templates/iis-2vm-sql-1vm/
+# - https://github.com/Azure/azure-quickstart-templates/tree/master/iis-2vm-sql-1vm
+# Author: Istvan Cebrian / Date: 17/01/2016
+# Version: 0.1
+######################################################################################
+
+$day = (Get-Date).Day
+
+$month = (Get-Date).Month
+$year = (Get-Date).Year
+$hour = (Get-Date).Hour
+$minute = (Get-Date).Minute
+$seconds = (Get-Date).Second
+
+$rgName = "cm-classic"
+$location = "East US"
+
+#Write-Host "Resource Group Name: $name"
+
+#Login-AzureRmAccount
+
+#New-AzureRmResourceGroup -Name $rgName -Location $location
+
+New-AzureRmResourceGroupDeployment -Name $rgName -ResourceGroupName $rgName -TemplateUri https://raw.githubusercontent.com/icebrian/azure/master/templates/iis-2vm-sql-1vm/azuredeploy.json -TemplateParameterUri https://raw.githubusercontent.com/icebrian/azure/master/templates/iis-2vm-sql-1vm/azuredeploy.parameters.json -Verbose
