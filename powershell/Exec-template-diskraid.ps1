8ec99a6bb509ec00c703f0b967cdc872dae8bc69
Istvan Cebrian <istvan.cebrian@gmail.com>
2016-03-15 16:12:26 +0000

    ps script changes

--- powershell/Exec-template-diskraid.ps1
+++ powershell/Exec-template-diskraid.ps1
@@ -1,4 +1,12 @@
-
+######################################################################################
+# Script: 2iis-1sql
+# Description: Uses Template file to create 2 Web Server VM's and 1 SQL VM
+# for further information, please refer to:
+# - https://azure.microsoft.com/en-us/documentation/templates/iis-2vm-sql-1vm/
+# - https://github.com/Azure/azure-quickstart-templates/tree/master/iis-2vm-sql-1vm
+# Author: Istvan Cebrian / Date: 17/01/2016
+# Version: 0.1
+######################################################################################
 
 $day = (Get-Date).Day
 
@@ -10,6 +18,8 @@ $seconds = (Get-Date).Second
 
 $rgName = "diskraid_$year$month$day-$hour$minute$seconds"
 $location = "West Europe"
+
+#Write-Host "Resource Group Name: $name"
 
 #Login-AzureRmAccount
 
