7d0f6096ece7d25c649b34bb081539f636727454
Istvan Cebrian <istvan.cebrian@gmail.com>
2016-04-29 12:26:55 +0100

    ps

--- powershell/Exec-template-with-Extension.ps1
+++ powershell/Exec-template-with-Extension.ps1
@@ -4,4 +4,6 @@
 
 Login-AzureRmAccount
 
+Get-AzureVMAvailableExtension | select ExtensionName,Publisher,Version,PublishedDate
+
 New-AzureRmResourceGroupDeployment -Name "cm-arm" -ResourceGroupName "cm-arm" -TemplateUri https://raw.githubusercontent.com/icebrian/azure/master/templates/puppet-agent-windows/azuredeploy.json -TemplateParameterUri https://raw.githubusercontent.com/icebrian/azure/master/templates/puppet-agent-windows/azuredeploy.parameters.json -Verbose
