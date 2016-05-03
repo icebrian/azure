f7ab47c26cb23084bd7e492740419974f5bb2e46
Istvan Cebrian <istvan.cebrian@gmail.com>
2016-05-03 15:01:53 +0100

    only 1 disk

--- templates/vm-multiple-data-disk-and-chef/DataDisk.ps1
+++ templates/vm-multiple-data-disk-and-chef/DataDisk.ps1
@@ -16,41 +16,5 @@ Configuration Server
                 DriveLetter = 'F'
                 DependsOn = '[xWaitforDisk]Disk'
             }
-
-            xWaitforDisk Disk2
-            {
-                DiskNumber = 1
-            }
-
-            xDisk Volume2
-            {
-                DiskNumber = 1
-                DriveLetter = 'G'
-                DependsOn = '[xWaitforDisk]Disk2'
-            }
-
-            xWaitforDisk Disk3
-            {
-                DiskNumber = 2
-            }
-
-            xDisk Volume3
-            {
-                DiskNumber = 2
-                DriveLetter = 'H'
-                DependsOn = '[xWaitforDisk]Disk3'
-            }
-
-            xWaitforDisk Disk4
-            {
-                DiskNumber = 3
-            }
-
-            xDisk Volume4
-            {
-                DiskNumber = 3
-                DriveLetter = 'I'
-                DependsOn = '[xWaitforDisk]Disk4'
-            }
         }
 }
