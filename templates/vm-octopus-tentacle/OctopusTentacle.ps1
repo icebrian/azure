5ff8044d6da169c997f60567b72fde5e2d9ebff2
Istvan Cebrian <istvan.cebrian@gmail.com>
2016-05-04 12:25:54 +0100

    initial

---
+++ templates/vm-octopus-tentacle/OctopusTentacle.ps1
@@ -0,0 +1,29 @@
+Configuration OctopusConfig
+{
+    param ($ApiKey, $OctopusServerUrl, $Environments, $Roles, $ListenPort)
+
+    Import-DscResource -Module OctopusDSC
+
+    Node "localhost"
+    {
+        cTentacleAgent OctopusTentacle
+        {
+            Ensure = "Present";
+            State = "Started";
+
+            # Tentacle instance name. Leave it as 'Tentacle' unless you have more
+            # than one instance
+            Name = "Tentacle";
+
+            # Registration - all parameters required
+            ApiKey = $ApiKey;
+            OctopusServerUrl = $OctopusServerUrl;
+            Environments = $Environments;
+            Roles = $Roles;
+
+            # Optional settings
+            ListenPort = $ListenPort;
+            DefaultApplicationDirectory = "C:\Applications"
+        }
+    }
+}
---
+++ templates/vm-octopus-tentacle/README.md
@@ -0,0 +1,9 @@
+# Create a Virtual Machine from a Windows Image and install/configura Octopus Tentacle
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+
+This template allows you to create a Windows Virtual Machine from a specified image during the template deployment. It also installs and configuress Octopus Tentacle acording to provided parameters.
+
+Note: Uses https://github.com/OctopusDeploy/OctopusDSC
