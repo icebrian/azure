7f1d6b8a7543d412177d82ea5a46983978bbf267
Istvan Cebiran <istvan.cebrian@gmail.com>
2017-08-15 21:14:21 +0100

    robocopy

---
+++ powershell/robocopy.ps1
@@ -0,0 +1,6 @@
+﻿
+$source_path = "C:\source\path"
+
+$destination_path = "\\192.71.31.90\c$\Storage\Vol\Data"
+
+robocopy $source_path $destination_path /MIR /MT:128 /FFT /Z /E /W:5 /R:10 /TEE /LOG:C:\temp\RoboCopyLog-new2.txt
