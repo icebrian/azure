0cc535c1cfb45955cdfb5abded74e0f3d673e6f2
Istvan Cebrian <istvan.cebrian@gmail.com>
2018-02-12 11:34:55 +0000

    rem

--- templates/azhpc_lustre/scripts/ssd-test1.sh
+++
@@ -1,8 +0,0 @@
-# Change this variable to the path of the device you want to test
-cd /mnt/sasdata1/
-
-##The following command performs 16 KB random write operations.
-fio --name=fio_test_file --direct=1 --rw=randwrite --bs=4k --size=256M --numjobs=8 --time_based --runtime=60 --group_reporting
-
-##The following command performs 16 KB random read operations.
-fio --name=fio_test_file --direct=1 --rw=randread --bs=4k --size=256M --numjobs=8 --time_based --runtime=60 --group_reporting
